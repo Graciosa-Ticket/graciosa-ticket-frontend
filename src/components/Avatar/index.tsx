5b5619efbd088567b747151b6378c95f27d0f7e8
Chefe Gordo <giovanigsilvano@gmail.com>
2024-12-06 15:11:14 -0300

    fix: amazonURL

--- src/components/Avatar/index.tsx
+++ src/components/Avatar/index.tsx
@@ -4,7 +4,7 @@ import styled from "styled-components";
 export const blankAvatar =
   "https://static.vecteezy.com/system/resources/thumbnails/036/280/651/small_2x/default-avatar-profile-icon-social-media-user-image-gray-avatar-icon-blank-profile-silhouette-illustration-vector.jpg";
 
-export const amazonURL = "https://tickets.graciosa.com.br/api/files/";
+export const amazonURL = "https://ticket.graciosa.com.br/api/files/";
 
 const AvatarContainer = styled.img`
   width: 35px;
